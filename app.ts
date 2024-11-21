5164c955aa3144031f46ebe8fa6ea4c3fc48dd00
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-11-21 22:36:47 +0200

    enable launcher, remove flake (added to main nix config)

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import { App } from "astal/gtk3"
 import style from "./style.scss"
-import Bar from "./widget/Bar2"
+import Bar from "./widget/Bar"
 import Applauncher from "./widget/Applauncher"
 
 App.start({
     css: style,
     main() {
         App.get_monitors().map(Bar)
+        Applauncher()
+        App.get_window("launcher")!.hide() // dont show launcher on startup
     },
 })
